e3b1491fe1c2add7e69e0eb3c12ed18463b8d05e
Daniël <danielmeijs@gmail.com>
2024-03-08 12:14:11 +0100

    Replaced Buffer operations with bit operations.

--- URDecoder.js
+++ URDecoder.js
@@ -9,40 +9,38 @@ TYPES.RELATIVE_HUMIDITY = 4;
  * @param input The received payload.
  * @returns {object}  Decoded object to be transformed to JSON by TTN.
 * */
-function decodeUplink(input){
-    //Buffer is created from the byte array inside input.
+export function decodeUplink(input) {
     //The byte array consist of 8-bit unsigned integers.
-    const buffer = Buffer.from(input.bytes);
+    const byteArray = input.bytes;
     //Create object to store decoded information.
     let decoded = {
         reefId: null,
         points: []
-    }
-    //Cursor to track our position in the buffer.
+    };
+
+    //Cursor is used to track the position in the byte array.
     let cursor = 0;
-    while (cursor < buffer.length){
-        //Read the byte at the current cursor position in the buffer and increment cursor.
-        //Check the type to define how to read the byte(s) after the type byte.
-        switch (buffer.readUInt8(cursor++)) {
+    while (cursor < byteArray.length) {
+        //Read the byte at the current cursor position in the array after increment cursor.
+        switch (byteArray[cursor++]) {
+            //Check the type to define how to read the byte(s) after the type byte.
             case TYPES.REEF:
                 //Reef value is stored in 1 byte.
-                //Increment  cursor after reading.
-                decoded.reefId = buffer.readUInt8(cursor++);
+                decoded.reefId = byteArray[cursor++]; //Increment  cursor after reading.
                 break;
             case TYPES.POINT:
                 //Point value is stored in 1 byte.
                 //If a point is flag is read push a new object into the points array.
-                decoded.points.push({position: buffer.readUInt8(cursor++)});
+                decoded.points.push({ position: byteArray[cursor++] });
                 break;
             case TYPES.TEMPERATURE:
-                /*
-                * Temperature is stored in 2 bytes as an 16-bit signed integer.
-                * Read 2 bytes and divide the value by 10 to transform it back into a 1-point decimal.
-                * example: 655 / 10 = 65.5
-                * Store the value in the last created point object.
-                * */
-                decoded.points[decoded.points.length - 1].temperature = buffer.readInt16BE(cursor) / 10;
-                cursor += 2; //increment cursor by 2 as we've read 2 bytes.
+                // To read temperature we need to combine 2 bytes into a 16-bit signed integer.
+                // Combine two bytes
+                let temperatureValue = (byteArray[cursor++] << 8) | byteArray[cursor++];
+                // If the value is negative, convert it to a signed integer
+                temperatureValue = temperatureValue < 0x8000 ? temperatureValue : temperatureValue - 0x10000;
+                // Divide by 10 to transform it back into a 1-point decimal
+                decoded.points[decoded.points.length - 1].temperature = temperatureValue / 10;
                 break;
             case TYPES.RELATIVE_HUMIDITY:
                 /*
@@ -51,12 +49,13 @@ function decodeUplink(input){
                 * During encoding, it is multiplied by 2 to make it a single byte integer.
                 * This can be done as it is measured in steps of 0.5
                 * */
-                decoded.points[decoded.points.length -1].humidity = buffer.readUInt8(cursor++) / 2;
+                decoded.points[decoded.points.length - 1].humidity = byteArray[cursor++] / 2;
                 break;
             default:
-                //if type byte is not recognised or empty do nothing.
-                break
+                // If type byte is not recognized or empty, do nothing.
+                break;
         }
     }
+
     return decoded;
 }
